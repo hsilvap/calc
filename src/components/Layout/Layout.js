d0e013372ac036d0f000d6e80aa4618493c4d0ce
Homero Silva <hsilvap@me.com>
2021-03-04 15:06:44 -0600

    clean up - installing libs - scaffolding

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,8 @@
+import { StyledLayout } from "./Layout.styled"
+
+const Layout = () => {
+    return <StyledLayout>
+    </StyledLayout>
+}
+
+ export default Layout
---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled'
+
+export const StyledLayout = styled.div`
+    display: flex;
+    align-items: center;
+    background-color:black;
+    width: 250px;
+    height: 400px;
+`
+ 
