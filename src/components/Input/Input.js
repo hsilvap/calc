470b2311034f2cf3892508bee3219343f1d16f02
Homero Silva <hsilvap@me.com>
2021-03-04 16:19:59 -0600

    add imput component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,9 @@
+import { StyledInput } from "./Input.styled"
+
+const Input = () => {
+    return <StyledInput value="132">
+        12332
+    </StyledInput>
+}
+
+export default Input
---
+++ src/components/Input/Input.styled.js
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled'
+
+export const StyledInput = styled.div`
+    font-size:100px;
+    color: white;
+`
