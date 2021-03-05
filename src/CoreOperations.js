bd899946c3c2c6b2af5ca15cc8d1a16cfdc7f247
Homero Silva <hsilvap@me.com>
2021-03-04 22:24:06 -0600

    refactor

--- src/CoreOperations.js
+++ src/CoreOperations.js
@@ -11,32 +11,34 @@ export function Calculate(operation, input, secondInput) {
             return Divide(input, secondInput)
         default:
             throw new Error('Invalid operation')
-
     }
 }
 
 function Add(input, secondInput) {
-    const pInput = parseFloat(input)
-    const pSecondInput = parseFloat(secondInput)
+    const [pInput, pSecondInput] = parseNumbers(input, secondInput)
     return pInput + pSecondInput
 }
 
 function Substract(input, secondInput) {
-    const pInput = parseFloat(input)
-    const pSecondInput = parseFloat(secondInput)
+    const [pInput, pSecondInput] = parseNumbers(input, secondInput)
     return pInput - pSecondInput
 }
 
 function Multiply(input, secondInput) {
-    const pInput = parseFloat(input)
-    const pSecondInput = parseFloat(secondInput)
+    const [pInput, pSecondInput] = parseNumbers(input, secondInput)
     return pInput * pSecondInput
 }
 
 function Divide(input, secondInput) {
-    const pInput = parseFloat(input)
-    const pSecondInput = parseFloat(secondInput)
+    const [pInput, pSecondInput] = parseNumbers(input, secondInput)
     if (pSecondInput === 0)
         return 'Error'
     return pInput / pSecondInput
 }
+
+function parseNumbers(input, secondInput) {
+    const pInput = parseFloat(input)
+    const pSecondInput = parseFloat(secondInput)
+    return [pInput, pSecondInput]
+}
+
