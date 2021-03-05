40d76ca6cdc70db12b62fdaac91bd7ff87dd4af5
Homero Silva <hsilvap@me.com>
2021-03-04 22:50:11 -0600

    fix numbers displayed

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,8 +4,23 @@ import { StyledInput } from "./Input.styled"
 
 const Input = () => {
     const { state } = useContext(StoreContext)
+
+    const formatResult = (amount) => {
+        let amountStr = amount.toString()
+        if (amount < 0) {
+            if (amountStr.length > 10) {
+                return amount.toFixed(10)
+            }
+        } else {
+            if (amountStr.length > 9) {
+                return +amountStr.substring(0, 9)
+            }
+        }
+        return amount
+    }
+
     return <StyledInput>
-        {state.displayValue !== 'Error' ? new Intl.NumberFormat().format(state.displayValue) : state.displayValue}
+        {state.displayValue !== 'Error' ? new Intl.NumberFormat(undefined, { maximumFractionDigits: 12 }).format(formatResult(state.displayValue)) : state.displayValue}
     </StyledInput>
 }
 
