5a97f66279c405a9e0c21b0998785e97df7d1f5a
Homero Silva <hsilvap@me.com>
2021-03-04 21:26:07 -0600

    accumulate calculator

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -7,6 +7,7 @@ export const StoreActions = {
     MULTIPLY: 'MULTIPLY',
     DIVIDE: 'DIVIDE',
     TAP: 'TAP',
+    CLEAR_ALL: 'CLEAR_ALL',
     CLEAR: 'CLEAR',
     RESULT: 'RESULT',
 }
@@ -18,30 +19,71 @@ export function StoreReducer(state, action) {
             if (state.operation === '') {
                 const current = state.firstValue + data.value
                 return { ...state, firstValue: current, displayValue: current }
-            } else {
-                const current = state.secondValue + data.value
-                return { ...state, secondValue: current, displayValue: current }
+            } else if (state.operation === '=') {
+                const current = data.value
+                return { ...state, firstValue: current, displayValue: current, operation: '' }
+            }
+            else {
+                if (state.firstValue === '') {
+                    const current = state.firstValue + data.value
+                    return { ...state, firstValue: current, displayValue: current }
+                } else {
+                    const current = state.secondValue + data.value
+                    return { ...state, secondValue: current, displayValue: current }
+                }
             }
             break
         }
         case StoreActions.ADD: {
+            const { firstValue, secondValue } = state
+            if (firstValue !== '' && secondValue !== '') {
+                const result = Calculate('+', firstValue, secondValue)
+                return { ...state, operation: '+', firstValue: result, secondValue: '', displayValue: result }
+            }
             return { ...state, operation: '+' }
         }
         case StoreActions.SUBSTRACT: {
+            const { firstValue, secondValue } = state
+            if (firstValue !== '' && secondValue !== '') {
+                const result = Calculate('-', firstValue, secondValue)
+                return { ...state, operation: '-', firstValue: result, secondValue: '', displayValue: result }
+            }
             return { ...state, operation: '-' }
         }
         case StoreActions.MULTIPLY: {
+            const { firstValue, secondValue } = state
+            if (firstValue !== '' && secondValue !== '') {
+                const result = Calculate('x', firstValue, secondValue)
+                return { ...state, operation: 'x', firstValue: result, secondValue: '', displayValue: result }
+            }
             return { ...state, operation: 'x' }
         }
         case StoreActions.DIVIDE: {
+            const { firstValue, secondValue } = state
+            if (firstValue !== '' && secondValue !== '') {
+                const result = Calculate('÷', firstValue, secondValue)
+                return { ...state, operation: '÷', firstValue: result, secondValue: '', displayValue: result }
+            }
             return { ...state, operation: '÷' }
         }
         case StoreActions.RESULT: {
             const { operation, firstValue, secondValue } = state
+            if (firstValue === '' || secondValue === '')
+                return { ...state, operation: '=' }
             const result = Calculate(operation, firstValue, secondValue)
             return { ...state, displayValue: result, operation: '', firstValue: '', secondValue: '' }
         }
         case StoreActions.CLEAR: {
+            const { firstValue, secondValue } = state
+            if (secondValue !== '') {
+                return { ...state, displayValue: '', secondValue: '' }
+            }
+            if (firstValue !== '') {
+                return { ...state, displayValue: '', firstValue: '' }
+            }
+            break;
+        }
+        case StoreActions.CLEAR_ALL: {
             return { ...initialState }
         }
         default: {
