e1388a3a6644ebcc089f78b3ac9738d9260b14a9
Homero Silva <hsilvap@me.com>
2021-03-04 19:54:09 -0600

    add core logic

---
+++ src/CoreOperations.js
@@ -0,0 +1,42 @@
+
+export function Calculate(operation, input, secondInput) {
+    switch (operation) {
+        case '+':
+            return Add(input, secondInput)
+        case '-':
+            return Substract(input, secondInput)
+        case 'x':
+            return Multiply(input, secondInput)
+        case '÷':
+            return Divide(input, secondInput)
+        default:
+            throw new Error('Invalid operation')
+
+    }
+}
+
+function Add(input, secondInput) {
+    const pInput = parseFloat(input)
+    const pSecondInput = parseFloat(secondInput)
+    return pInput + pSecondInput
+}
+
+function Substract(input, secondInput) {
+    const pInput = parseFloat(input)
+    const pSecondInput = parseFloat(secondInput)
+    return pInput - pSecondInput
+}
+
+function Multiply(input, secondInput) {
+    const pInput = parseFloat(input)
+    const pSecondInput = parseFloat(secondInput)
+    return pInput * pSecondInput
+}
+
+function Divide(input, secondInput) {
+    const pInput = parseFloat(input)
+    const pSecondInput = parseFloat(secondInput)
+    if (pSecondInput == 0)
+        return 'Error'
+    return pInput / pSecondInput
+}
